d580b083ebf9ebd6c3473acac6a243e4eccdc4a9
chakchakk <ioann.chimrov@gmail.com>
2022-10-12 10:08:54 +0300

    change name for test

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -7,7 +7,8 @@ class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_error_page(self):
+    def test_page_not_found_uses_correct_template(self):
+        """Ошибка 404 возвращает кастомный шаблон core/404.html."""
         response = self.client.get('/nonexists-page/')
 
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
